112012a37873afd642eaf970618dd13158fcfc81
rodrigodasilva <ribeiro.rodrigodasilva>
2019-08-25 20:19:16 -0300

    feat: estilizacao pag. autenticacao

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 // import { Container } from './styles';
 
+import logo from '~/assets/logo.png';
+
 export default function SignIn() {
-  return <h1>SigIn</h1>;
+  return (
+    <>
+      <img src={logo} alt="MeetApp" />
+
+      <form>
+        <input type="email" placeholder="Digite seu email" />
+        <input type="password" placeholder="Digite sua senha" />
+
+        <button type="submit">Entrar</button>
+        <Link to="/register">Criar conta gratuita</Link>
+      </form>
+    </>
+  );
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 // import { Container } from './styles';
 
+import logo from '~/assets/logo.png';
+
 export default function SignUp() {
-  return <h1>SigUp</h1>;
+  return (
+    <>
+      <img src={logo} alt="Gobarber" />
+
+      <form>
+        <input type="text" placeholder="Seu nome" />
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha secreta" />
+
+        <button type="submit">Criar conta</button>
+        <Link to="/">Já tenho login</Link>
+      </form>
+    </>
+  );
 }
