d29ec5f83c7639d7b2f40786802fccf731e71bb7
rodrigodasilva <ribeiro.rodrigodasilva>
2019-08-25 22:46:08 -0300

    feat: requisicoes autenticadas

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 
 // import { Container } from './styles';
 
+import api from '~/services/api';
+
 export default function Dashboard() {
+  const response = api.get('meetups');
+
+  console.tron.log(response);
+
   return <h1>Dashboard</h1>;
 }
