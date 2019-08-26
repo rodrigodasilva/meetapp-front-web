745613fe89ad77bf34487b80b2264ecf69d5f59f
rodrigodasilva <ribeiro.rodrigodasilva>
2019-08-25 22:05:40 -0300

    feat: mensagem com toastify

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,4 +1,5 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
 import history from '~/services/history';
 import api from '~/services/api';
@@ -16,15 +17,11 @@ export function* signIn({ payload }) {
 
     const { token, user } = response.data;
 
-    if (!user) {
-      console.tron.error('Usuário não cadastrado!');
-      return;
-    }
-
     yield put(signInSuccess(token, user));
 
     history.push('/dashboard');
   } catch (err) {
+    toast.error('Falha na autenticação, verifique seus dados');
     yield put(signFailure());
   }
 }
