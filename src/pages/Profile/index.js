cc9fb1639c57b798b51ae12a5b9e98cc14f8e0df
rodrigodasilva <ribeiro.rodrigodasilva>
2019-10-23 21:01:01 -0300

    refatory: profile page styles

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Form, Input } from '@rocketseat/unform';
 import { Spinner } from 'react-activity';
 
 import { updateProfileRequest } from '~/store/modules/user/actions';
 
-import { Container } from './styles';
+import { Container, FormUnform, InputUnform, ButtonSubmit } from './styles';
 
 export default function Profile() {
   const profile = useSelector(state => state.user.profile);
@@ -19,30 +18,36 @@ export default function Profile() {
 
   return (
     <Container>
-      <Form initialData={profile} onSubmit={handleSubmit}>
-        <Input name="name" placeholder="Nome completo" />
-        <Input name="email" type="email" placeholder="Seu endereço de e-mail" />
+      <FormUnform initialData={profile} onSubmit={handleSubmit}>
+        <InputUnform name="name" placeholder="Nome completo" />
+        <InputUnform
+          name="email"
+          type="email"
+          placeholder="Seu endereço de e-mail"
+        />
 
         <hr />
 
-        <Input
+        <InputUnform
           type="password"
           name="oldPassword"
           placeholder="Sua senha atual"
         />
-        <Input type="password" name="password" placeholder="Sua nova senha" />
-        <Input
+        <InputUnform
+          type="password"
+          name="password"
+          placeholder="Sua nova senha"
+        />
+        <InputUnform
           type="password"
           name="confirmPassword"
           placeholder="Confirme sua nova senha"
         />
 
-        <div>
-          <button type="submit">
-            {loading ? <Spinner color="#fff" size={14} /> : <>Salvar perfil</>}
-          </button>
-        </div>
-      </Form>
+        <ButtonSubmit type="submit">
+          {loading ? <Spinner color="#fff" size={14} /> : <>Salvar perfil</>}
+        </ButtonSubmit>
+      </FormUnform>
     </Container>
   );
 }
--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -1,9 +1,13 @@
 import styled from 'styled-components';
-import { darken } from 'polished';
+
+import { Form, Input } from '@rocketseat/unform';
+import Button from '~/components/Button';
 
 export const Container = styled.div`
   max-width: 940px;
   margin: 50px auto;
+  display: flex;
+  flex-direction: column;
 
   @media (max-width: 980px) {
     padding: 0 30px;
@@ -12,72 +16,53 @@ export const Container = styled.div`
   @media (max-width: 360px) {
     padding: 0 15px;
   }
+`;
 
-  form {
-    display: flex;
-    flex-direction: column;
-    margin-top: 30px;
+export const FormUnform = styled(Form)`
+  display: flex;
+  flex-direction: column;
+  margin-top: 30px;
 
-    input {
-      background: rgba(0, 0, 0, 0.1);
-      border: 0;
-      border-radius: 4px;
-      height: 44px;
-      padding: 0 15px;
-      color: #fff;
-      margin: 0 0 10px;
+  /* Span Error  */
+  & span {
+    color: #f94d6a;
+    align-self: flex-start;
+    margin: 0 0 10px;
+    font-weight: bold;
+  }
 
-      &::placeholder {
-        color: rgba(255, 255, 255, 0.7);
-      }
-    }
+  & hr {
+    border: 0;
+    height: 1px;
+    background: rgba(255, 255, 255, 0.1);
+    margin: 20px 0 20px;
+  }
+`;
 
-    span {
-      color: #f94d6a;
-      align-self: flex-start;
-      margin: 0 0 10px;
-      font-weight: bold;
-    }
+export const InputUnform = styled(Input)`
+  background: rgba(0, 0, 0, 0.1);
+  border: 0;
+  border-radius: 4px;
+  height: 44px;
+  padding: 0 15px;
+  color: #fff;
+  margin: 0 0 10px;
 
-    hr {
-      border: 0;
-      height: 1px;
-      background: rgba(255, 255, 255, 0.1);
-      margin: 20px 0 20px;
-    }
+  &::placeholder {
+    color: rgba(255, 255, 255, 0.7);
+  }
 
-    div {
-      display: flex;
-      justify-content: flex-end;
+  &:focus {
+    border-left: 3px solid #f94d6a;
+  }
+`;
 
-      button {
-        margin: 5px 0;
-        height: 42px;
-        width: 162px;
-        background: #f94d6a;
-        border: 0;
-        border-radius: 4px;
-        margin-top: 15px;
+export const ButtonSubmit = styled(Button)`
+  width: 162px;
+  margin-top: 15px;
+  align-self: flex-end;
 
-        transition: background 0.2s;
-
-        display: flex;
-        flex-direction: row;
-        justify-content: center;
-        align-items: center;
-
-        font-weight: bold;
-        color: #fff;
-        font-size: 16px;
-
-        &:hover {
-          background: ${darken(0.03, '#F94D6A')};
-        }
-
-        @media (max-width: 375px) {
-          width: 100%;
-        }
-      }
-    }
+  @media (max-width: 375px) {
+    width: 100%;
   }
 `;
